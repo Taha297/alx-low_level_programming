a06ec78ac931b5e0e8b0a008bb38ccc756b313da
taha <taha@gmail.com>
2023-09-07 17:54:59 -0700

    mmm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <unistd.h>
 
 /**
@@ -13,21 +12,3 @@ int main(void)
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 858d1c32628c7f753ca70344d7d101ccc77abd53
-=======
-#include <stdlib.h>
-
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
->>>>>>> 419bf1b17aae4c6741137a137b037c98cff2e51c
