6b8c9797a9fbacc50cc89ca404ecb76092eb152d
taha <taha@gmail.com>
2023-09-07 17:50:39 -0700

    mm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 /**
 * main - Entry point
@@ -14,31 +13,3 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 858d1c32628c7f753ca70344d7d101ccc77abd53
-=======
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
-}
->>>>>>> 419bf1b17aae4c6741137a137b037c98cff2e51c
