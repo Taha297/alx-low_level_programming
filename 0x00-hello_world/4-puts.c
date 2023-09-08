0d0e94740db312da451bc20c330c4c67a58ea614
taha <taha@gmail.com>
2023-09-07 17:41:50 -0700

    mm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 #include<stdio.h>
-=======
-#include <stdio.h>
-
->>>>>>> 419bf1b17aae4c6741137a137b037c98cff2e51c
 /**
  * main - Entry point
  *
@@ -14,10 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 858d1c32628c7f753ca70344d7d101ccc77abd53
-=======
->>>>>>> 419bf1b17aae4c6741137a137b037c98cff2e51c
