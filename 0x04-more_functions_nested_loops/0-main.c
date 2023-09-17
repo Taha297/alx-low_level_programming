def6ca7a150b30d07a6d745d15e95a88fbc3e469
taha <taha@gmail.com>
2023-09-17 13:07:37 -0700

    mod

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
 #include "main.h"
 #include <stdio.h>
 
 /**
  * main - check the code.
-=======
-#include "holberton.h"
-#include <stdio.h>
-
-/**
- * main - check the code for Holberton School students.
->>>>>>> 419bf1b17aae4c6741137a137b037c98cff2e51c
  *
  * Return: Always 0.
  */
 int main(void)
 {
-<<<<<<< HEAD
     char c;
 
     c = 'A';
@@ -25,14 +16,3 @@ int main(void)
     printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
-
-=======
-  char c;
-
-  c = 'A';
-  printf("%c: %d\n", c, _isupper(c));
-  c = 'a';
-  printf("%c: %d\n", c, _isupper(c));
-  return (0);
-}
->>>>>>> 419bf1b17aae4c6741137a137b037c98cff2e51c
